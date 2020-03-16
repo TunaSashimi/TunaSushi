16e8b84cdcf406df9942b0a8019794b51b6bec84
jinsheng <jinsheng@didichuxing.com>
2020-03-16 23:37:25 +0800

    feat TView

---
+++ tunasushi/src/main/java/com/tunasushi/tuna/TBinding.java
@@ -0,0 +1,16 @@
+package com.tunasushi.tuna;
+
+import androidx.databinding.BindingAdapter;
+
+/**
+ * @author TunaSashimi
+ * @date 2020-03-16 23:34
+ * @Copyright 2020 TunaSashimi. All rights reserved.
+ * @Description
+ */
+public class TBinding {
+    @BindingAdapter({"url"})
+    public static void content(TView t, String content) {
+        t.setContent(content);
+    }
+}
