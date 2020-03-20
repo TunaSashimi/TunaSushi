dbe3e7a9e2e8c66f1b3de71a2d2b1b17c4d46606
jinsheng <jinsheng@didichuxing.com>
2020-03-20 13:59:54 +0800

    feat TView

--- tunasushi/src/main/java/com/tunasushi/tuna/TBinding.java
+++ tunasushi/src/main/java/com/tunasushi/tuna/TBinding.java
@@ -12,11 +12,19 @@ import android.databinding.BindingAdapter;
 public class TBinding {
     @BindingAdapter({"text"})
     public static void setText(TView t, String text) {
+        final String oldText = t.getText();
+        if ((text == null && oldText.length() == 0) || text == oldText || text.equals(oldText)) {
+            return;
+        }
         t.setText(text);
     }
 
     @BindingAdapter({"content"})
     public static void setContent(TView t, String content) {
+        final String oldContent = t.getContent();
+        if ((content == null && oldContent.length() == 0) || content == oldContent || content.equals(oldContent)) {
+            return;
+        }
         t.setContent(content);
     }
 }
